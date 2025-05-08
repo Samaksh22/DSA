1dc4fe58d56faac7d9b3cf5911a72f0979ac5a27
Samaksh <samakshsethiya22@gmail.com>
2025-05-08 14:40:51 +0530

    "hello"

---
+++ pointers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // a pointer holds the address of some variable
+    // '&' <= this represents the address-of opreator
+    // '*' <= this is used for derefrencing
+}
